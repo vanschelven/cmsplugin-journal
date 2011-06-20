e04a402d02c98a7feb6eaeb889cb86b48e979d88
Klaas van Schelven <klaas@vanschelven.com>
2011-06-20 16:35:27 +0200

    More files from cmsplugin-journal-0.1.tar.gz

---
+++ README.txt
@@ -0,0 +1,8 @@
+Journal
+-------
+
+This is a plugin for django-cms 2.0.
+
+This is a fork of cmsplugin_news by Harro van der Klauw <hvdklauw@gmail.com>
+http://bitbucket.org/MrOxiMoron/cmsplugin-news/
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmsplugin-journal',
+    version='0.1',
+    description='This is a news app/plugin for the django-cms 2',
+    author='Fajran Iman Rusadi',
+    author_email='fajran@gmail.com',
+    url='http://bitbucket.org/fajran/journal/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
